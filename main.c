1b029f9c614a0eb2237bbdac377970f8f767017f
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-04-17 12:58:26 +0300

    Update main

--- main.c
+++ main.c
@@ -9,10 +9,17 @@ int main(int argc, const char *argv[]) {
   Chunk chunk;
   initChunk(&chunk);
   int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  writeChunk(&chunk, OP_CONSTANT, 1);
+  writeChunk(&chunk, constant, 1);
+  writeChunk(&chunk, OP_RETURN, 2);
+  writeChunk(&chunk, OP_RETURN, 2);
+  writeChunk(&chunk, OP_RETURN, 2);
+  writeChunk(&chunk, OP_RETURN, 4);
+  writeChunk(&chunk, OP_CONSTANT, 4);
+  writeChunk(&chunk, constant, 4);
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
   freeChunk(&chunk);
   return 0;
 }
+

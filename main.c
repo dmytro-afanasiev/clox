ed0f7dc2c7d25f62faefb9951bc56dee6af2d269
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-04-19 15:20:49 +0300

    Add vm

--- main.c
+++ main.c
@@ -1,24 +1,24 @@
 #include "chunk.h"
 #include "common.h"
 #include "debug.h"
+#include "vm.h"
 
 // TODO: if opconstant has two bytes: one for op and second for index. Does it
 // mean that there is only 256 constants available
 
 int main(int argc, const char *argv[]) {
+  initVM();
   Chunk chunk;
   initChunk(&chunk);
   int constant = addConstant(&chunk, 1.2);
   writeChunk(&chunk, OP_CONSTANT, 1);
   writeChunk(&chunk, constant, 1);
-  writeChunk(&chunk, OP_RETURN, 2);
-  writeChunk(&chunk, OP_RETURN, 2);
-  writeChunk(&chunk, OP_RETURN, 2);
-  writeChunk(&chunk, OP_RETURN, 4);
   writeChunk(&chunk, OP_CONSTANT, 4);
   writeChunk(&chunk, constant, 4);
   writeChunk(&chunk, OP_RETURN, 123);
   disassembleChunk(&chunk, "test chunk");
+  interpret(&chunk);
+  freeVM();
   freeChunk(&chunk);
   return 0;
 }

fa2dfd1ab103dced79c69eb8d4e5e8d72aa002a4
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-04-20 15:44:14 +0300

    Use writeConstant

--- main.c
+++ main.c
@@ -6,19 +6,13 @@ int main(int argc, const char *argv[]) {
   initVM();
   Chunk chunk;
   initChunk(&chunk);
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  writeConstant(&chunk, 1.2, 123);
 
-  constant = addConstant(&chunk, 3.4);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  writeConstant(&chunk, 3.4, 123);
 
   writeChunk(&chunk, OP_ADD, 123);
 
-  constant = addConstant(&chunk, 5.6);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
+  writeConstant(&chunk, 5.6, 123);
 
   writeChunk(&chunk, OP_DIVIDE, 123);
   writeChunk(&chunk, OP_NEGATE, 123);

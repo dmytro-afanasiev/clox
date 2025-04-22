766285dccc69e06c4f98b358ce3cc5a14177530e
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-04-22 21:40:06 +0300

    Add strings

--- compiler.c
+++ compiler.c
@@ -4,6 +4,7 @@
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
+#include "object.h"
 
 #ifdef DEBUG_PRINT_CODE
 #include "debug.h"
@@ -163,6 +164,9 @@ static void number() {
   double value = strtod(parser.previous.start, NULL);
   emitConstant(NUMBER_VAL(value));
 }
+static void string() {
+  emitConstant(OBJ_VAL(copyString(parser.previous.start+1, parser.previous.length - 2)));
+}
 static void unary() {
   TokenType operatorType = parser.previous.type;
   parsePrecedence(PREC_UNARY);
@@ -198,7 +202,7 @@ ParseRule rules[] = {
     [TOKEN_LESS] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_LESS_EQUAL] = {NULL, binary, PREC_COMPARISON},
     [TOKEN_IDENTIFIER] = {NULL, NULL, PREC_NONE},
-    [TOKEN_STRING] = {NULL, NULL, PREC_NONE},
+    [TOKEN_STRING] = {string, NULL, PREC_NONE},
     [TOKEN_NUMBER] = {number, NULL, PREC_NONE},
     [TOKEN_AND] = {NULL, NULL, PREC_NONE},
     [TOKEN_CLASS] = {NULL, NULL, PREC_NONE},
--- value.c
+++ value.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <string.h>
 
 #include "memory.h"
+#include "object.h"
 #include "value.h"
 
 void initValueArray(ValueArray *array) {
@@ -34,6 +36,9 @@ void printValue(Value value) {
   case VAL_NUMBER:
     printf("%g", AS_NUMBER(value));
     break;
+  case VAL_OBJ:
+    printObject(value);
+    break;
   }
 }
 bool valuesEqual(Value a, Value b) {
@@ -44,6 +49,12 @@ bool valuesEqual(Value a, Value b) {
     return AS_BOOL(a) == AS_BOOL(b);
   case VAL_NUMBER:
     return AS_NUMBER(a) == AS_NUMBER(b);
+  case VAL_OBJ: {
+    ObjString *aString = AS_STRING(a);
+    ObjString *bString = AS_STRING(b);
+    return aString->length == bString->length &&
+           memcmp(aString->chars, bString->chars, aString->length) == 0;
+  }
   case VAL_NIL:
     return true;
   default:

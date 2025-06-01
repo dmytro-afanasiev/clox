f2fc846e3d5d59f8f434947049196c53832be9bb
Dmytro Afanasiev <Dmytro_Afanasiev1@epam.com>
2025-06-01 12:04:46 +0300

    Use bitwise and instead of module of power of two

--- table.c
+++ table.c
@@ -19,7 +19,7 @@ void freeTable(Table *table) {
   initTable(table);
 }
 static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry *tombstone = NULL;
   for (;;) {
     Entry *entry = &entries[index];
@@ -33,7 +33,7 @@ static Entry *findEntry(Entry *entries, int capacity, ObjString *key) {
     } else if (entry->key == key) {
       return entry;
     }
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 bool tableGet(Table *table, ObjString *key, Value *value) {
@@ -102,7 +102,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
                            uint32_t hash) {
   if (table->count == 0)
     return NULL;
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
   for (;;) {
     Entry *entry = &table->entries[index];
     if (entry->key == NULL) {
@@ -112,7 +112,7 @@ ObjString *tableFindString(Table *table, const char *chars, int length,
                memcmp(entry->key->chars, chars, length) == 0) {
       return entry->key;
     }
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 void tableRemoveWhite(Table *table) {
